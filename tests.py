ef4da0acdde1a269a0f763c3a5a1863d326acfbd
Svajunas900 <svajunaskoncius11@gmail.com>
2025-02-07 13:21:21 +0200

    Change hardcoded value

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
 import requests
 import pytest
 import requests_mock
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+
+USERNAME_TESTING = os.getenv("USERNAME_TESTING")
+PASSWORD_TESTING = os.getenv("PASSWORD_TESTING")
 
 @pytest.fixture
 def mock_requests():
@@ -9,10 +16,10 @@ def mock_requests():
         yield m
 
 def test_login_in(mock_requests):
-  data = {"username": "Svajunas",
-          "password": "koncius"}
+  data = {"username": USERNAME_TESTING,
+          "password": PASSWORD_TESTING}
   mock_requests.get(f'/', json=data)
-  assert '{"username": "Svajunas", "password": "koncius"}' == requests.get("thtp://localhost:8000/").text
+  assert f'{"username": {USERNAME_TESTING}, "password": {PASSWORD_TESTING}}' == requests.get("thtp://localhost:8000/").text
 
 
 
